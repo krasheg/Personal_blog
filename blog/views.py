5018478ca2a1e5f5b6fcf1f5283bbbd248510255
INlOEtdF <krasheg@gmail.com>
2023-03-13 11:24:42 +0200

    added views, urls for comments. Created html files

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from blog.models import Post, Comment
-from blog.forms import PostForm
+from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView, UpdateView, DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 
 class AboutView(TemplateView):
@@ -49,3 +51,44 @@ class DraftListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
+
+######################################
+######################################
+
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
